d9f5eabc4c608bd4d5b96aea2a812065e4a131e9
Gagandeep Singh <gagandeepisheresince2004@gmail.com>
2025-07-19 17:03:02 +0530

    URL added to cors

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -5,5 +5,6 @@ const allowedOrigins = [
   "http://127.0.0.1:3000",
   "http://127.0.0.1:5173",
   "http://127.0.0.1:5174",
+  "  https://lms-1-lzu6.onrender.com/",
 ];
 module.exports = allowedOrigins;
